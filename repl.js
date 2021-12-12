a3b8a48ae35dea4f03fdbf4dbdff7f56a7dfb7cb
Albert Rodriguez <albert@oikos.cash>
2021-12-11 20:37:12 -0400

    update repl

--- repl.js
+++ repl.js
@@ -29,7 +29,7 @@ console.log('Use the PRIVATE_KEY environment variable to set the private key.');
 
 const context = repl.start('> ').context;
 const ctx = {
-  oks: oksjs,
+  snx: oksjs,
   BigNumber,
   b32: oksjs.ethers.utils.formatBytes32String,
   pb32: oksjs.ethers.utils.parseBytes32String,
